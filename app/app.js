f0ffa0897b2738ce1260c805e294424ace08111d
Nitin Bisht <nitin.bisht@iongroup.com>
2020-05-07 19:22:51 +0530

    Services added

--- app/app.js
+++ app/app.js
@@ -1,7 +1,10 @@
 'use strict';
 
 // Declare app level module which depends on views, and core components
-var myApp = angular.module('myApp', [])
+var myApp = angular.module('myApp', [
+  'ngRoute',
+  'ngResource'
+])
 
 myApp.controller('MainController', ['$scope', function ($scope) {
   this.search = "Search..";
